0aa4a7801b0bb81da918bd37bbf685dbc49d8d28
Dayong Hyun <gusek93@naver.com>
2022-06-05 17:19:56 +0900

    memberhsip 중복 테스트

--- src/main/java/com/example/tddtest/repository/MembershipRepository.java
+++ src/main/java/com/example/tddtest/repository/MembershipRepository.java
@@ -1,9 +1,11 @@
 package com.example.tddtest.repository;
 
 import com.example.tddtest.domain.Membership;
+import com.example.tddtest.domain.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
 }
