f69ffa3e992c41b840f37621da25376be6e9b752
Dayong Hyun <gusek93@naver.com>
2022-06-06 02:25:09 +0900

    등록 성공 test

--- src/main/java/com/example/tddtest/controller/MembershipController.java
+++ src/main/java/com/example/tddtest/controller/MembershipController.java
@@ -26,8 +26,9 @@ public class MembershipController {
             @RequestHeader(USER_ID_HEADER) final String userId,
             @RequestBody final @Valid MembershipRequest membershipRequest) {
 
-        membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
+        final MembershipResponse membershipResponse = membershipService.addMembership(userId, membershipRequest.getMembershipType(), membershipRequest.getPoint());
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(membershipResponse);
     }
 }
--- src/test/java/com/example/tddtest/controller/MembershipControllerTest.java
+++ src/test/java/com/example/tddtest/controller/MembershipControllerTest.java
@@ -5,6 +5,7 @@ import com.example.tddtest.exception.GlobalExceptionHandler;
 import com.example.tddtest.exception.MembershipErrorResult;
 import com.example.tddtest.exception.MembershipException;
 import com.example.tddtest.service.MembershipRequest;
+import com.example.tddtest.service.MembershipResponse;
 import com.example.tddtest.service.MembershipService;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +21,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
+import java.nio.charset.StandardCharsets;
+
 import static com.example.tddtest.controller.MembershipConstants.USER_ID_HEADER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -86,6 +91,38 @@ public class MembershipControllerTest {
 
         //then
         resultActions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void 멤버십등록성공() throws Exception {
+        // given
+        final String url = "/api/v1/memberships";
+        final MembershipResponse membershipResponse = MembershipResponse.builder()
+                .id(-1L)
+                .membershipType(MembershipType.NAVER).build();
+
+        doReturn(membershipResponse).when(membershipService).addMembership("12345", MembershipType.NAVER, 10000);
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .header(USER_ID_HEADER, "12345")
+                        .content(gson.toJson(membershipRequest(10000, MembershipType.NAVER)))
+                        .contentType(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+                resultActions.andExpect(status().isCreated());
+
+        final MembershipResponse response = gson.fromJson(resultActions.andReturn()
+                .getResponse()
+                .getContentAsString(StandardCharsets.UTF_8), MembershipResponse.class);
+
+
+
+        assertThat(response.getId()).isNotNull();
+        assertThat(response.getMembershipType()).isEqualTo(MembershipType.NAVER);
+
     }
 
 }
