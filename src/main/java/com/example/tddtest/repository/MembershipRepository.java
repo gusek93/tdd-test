a8ce124475986398e7a34b2ef3424add154bda1f
Dayong Hyun <gusek93@naver.com>
2022-06-06 21:42:31 +0900

    membership 조회 테스트

--- src/main/java/com/example/tddtest/repository/MembershipRepository.java
+++ src/main/java/com/example/tddtest/repository/MembershipRepository.java
@@ -5,7 +5,11 @@ import com.example.tddtest.domain.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
+
+    List<Membership> findAllByUserId(final String userID);
 }
